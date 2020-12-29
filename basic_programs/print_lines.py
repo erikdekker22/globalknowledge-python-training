9eac3ee3ae386ab8962aec0feb2123fed37e88ea
Erik Dekker <erik.dekker@routz.nl>
2020-12-29 10:52:04 +0100

    Pretty Lines and Pretty Print Programs added

---
+++ basic_programs/print_lines.py
@@ -0,0 +1,26 @@
+def print_stars_line_length_ten():
+    '''Print een horizontale lijn van 10 sterren.'''
+    print('**********')
+
+def print_stars_line(length):
+    '''Print een horizontale lijn van `length` aantal sterren.'''
+    print('*' * length)
+
+def print_line(length, character):
+    '''Print een horizontale lijn van length aantal characters. '''
+    print(character * length)
+
+def main():
+    line_length = 25
+
+    print_stars_line_length_ten()
+    print_stars_line(line_length)
+    print_stars_line(line_length + 5)
+
+    char = '#'
+    print_line(line_length, character="%")
+    print_line(length=line_length, character=char)
+
+
+if __name__ == "__main__":
+    main()
