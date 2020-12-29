b7fb9c82bbb3fc265ceeb970233890e20af7c36c
Erik Dekker <erik.dekker@routz.nl>
2020-12-29 14:16:57 +0100

    Minor changes

--- basic_programs/authenticatie.py
+++ basic_programs/authenticatie.py
@@ -1,34 +1,30 @@
 import sys
 
-
 def ask_username():
     username = input("please type your username: ").strip()
-    if ('    ' in username ) or (' ' in username):
+    if ('    ' in username ) or (' ' in username): # provide error if there are spaces or tabs in username
         print("Your username may not contain tabs or spaces")
         sys.exit(42)
-    elif len(username) <= 3:
+    elif len(username) <= 3: # provide error if username is equal or less than 3 characters
         print("Your username is to short!")
         sys.exit(1)
-    else:
+    else: # else it is OK
         return username
-
 
 def ask_password(username):
     password = input("please type your password: ")
-    if len(password) <= 5:
+    if len(password) <= 5: # provide error if password is equal or less than 5 characters
         print("Your password is to short!")
         sys.exit(46)
-    elif password == username:
+    elif password == username: # Provide error when the password and username are the same
         print("Password and username cannot be the same! ")
         sys.exit(45)
-    else:
+    else: # else it is OK
         return password
 
-
 def is_welcome(name, passwd):
-    if (name != None) and (passwd != None):
+    if (name != None) and (passwd != None): # if it passed the previous tests it is oke, thus if both values are not None
         return True  # fixture, replace
-
 
 def main():
     username = ask_username()
@@ -41,7 +37,6 @@ def main():
         message = "Not allowed."
         print(message)
     sys.exit(0)
-
 
 if __name__ == "__main__":
     main()
--- basic_programs/menu.py
+++ basic_programs/menu.py
@@ -10,17 +10,17 @@ def quitting():
     print("Quiting")
 
 def what_to_do(value):
-    # value = input("What to do? [load, save, quit] ")
-    if value == 'load':
+    # value = input("What to do? [load, save, quit] ") # --> could be done instead of sys.argv
+    if value == 'load': # is the value equal to load?
         load_confuration()
-    elif value == 'save':
+    elif value == 'save': # is the value equal to save?
         save_configuration()
-    elif value == 'quit':
+    elif value == 'quit': # is the value equal to quit?
         quitting()
-    else:
-        print("This is highly unsual")
+    else: # in all other cases
+        print("This is highly unsual!? This was not one of the options you could give. ")
 
 def main():
-    what_to_do(sys.argv[1])
+    what_to_do(sys.argv[1]) # give the python file an argument on the terminal, e.g., "menu.py quit" or "menu.py save"
 
 main()
