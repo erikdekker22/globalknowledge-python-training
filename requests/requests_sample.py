412dbba7a5437c7babedd77e137b39c4a3657123
Erik Dekker <erik.dekker@routz.nl>
2020-12-29 16:20:06 +0100

    Changed requests_sample.py

--- requests/requests_sample.py
+++ requests/requests_sample.py
@@ -3,31 +3,30 @@ import json
 import requests
 
 ## Example kenteken: 93JGJB
-
-def ask_kenteken(): # -->
+def construct_url():
     kenteken = input("Please specify the kenteken you want to research: ")
-    return kenteken
+    url = 'https://opendata.rdw.nl/resource/m9d7-ebf2.json?kenteken=' + kenteken
+    return url
 
-def main(kenteken):
+def http_get(url):
+    response = requests.get(url)
+    return response
+
+def main():
     '''
     Please give a kenteken as input.
     The output is more information about the specific car.
     '''
-    url = 'https://opendata.rdw.nl/resource/m9d7-ebf2.json?kenteken=' + kenteken
-    response = requests.get(url)
+    url = construct_url()
+    response = http_get(url)
 
-    # test for success
     if response.status_code == 200:  # successfull response
         print(response.json)  # json string
-        data = json.loads(response.text) # convert to python dict with loads. Loads converts it from a JSON string. Load converts JSON from a file.
+        data = json.loads(response.text)  # convert to python dict with loads.
         print('You asked for information about kenteken ' + data[0]["kenteken"])
         print('The color of this car is: ' + data[0]["eerste_kleur"])
         print('The maximum amout of persons that can be in this car are: ' + data[0]["aantal_zitplaatsen"])
-
-        # for object in data:  # iterate through the JSON data object
-        #     for key, value in object.items():
-        #         print(key)
     else:
         print("Could not fetch data from", url)
 
-main(ask_kenteken())
+main()
