bbefeda1d9cc67dfd7b50d99fca97888746497a9
Erik Dekker <erik.dekker@routz.nl>
2020-12-31 10:58:06 +0100

    Added max address regex to find_addredd.py

--- cisco/find_address.py
+++ cisco/find_address.py
@@ -1,17 +1,25 @@
 import re
 
 def filter_internet_address(filename):
-    pattern = re.compile('((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)')
+    ip_pattern = re.compile('((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)')
     with open(filename, 'r') as arp_file:
         for line in arp_file:
-            match = re.search(pattern, line)
+            match = re.search(ip_pattern, line)
             if match:
                 print(match.group())
 
+def filter_mac_address(filename):
+    mac_pattern = re.compile('([0-9a-fA-F]){4}\.([0-9a-fA-F]){4}\.([0-9a-fA-F]){4}')
+    with open(filename, 'r') as arp_file:
+        for line in arp_file:
+            match = re.search(mac_pattern, line)
+            if match:
+                print(match.group())
 
 def main():
     filename = "arp_list.txt"
     filter_internet_address(filename)
+    filter_mac_address(filename)
 
 if __name__ == "__main__":
     main()
